0235bcb508d0d55a0b2a2e08ac28c5a268acfbe5
kiranwaghamode <kiranwaghamode99@gmail.com>
2026-09-10 07:43:38 +0530

    resolved google file issue

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ app.use(express.json());
 
 // Google authentication
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./google-credentials.json",
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
+
   scopes: [
     "https://www.googleapis.com/auth/spreadsheets",
   ],
